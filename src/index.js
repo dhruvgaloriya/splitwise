8cb6e276448dc28ac704e08474894c6b2be85053
Dhruvit Galoriya <dhruvitgaloriya@gmail.com>
2022-11-21 19:28:30 +0530

    push

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-import axe from "@axe-core/react";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-
-if (process.env.NODE_ENV !== "production") {
-  axe(React, ReactDOM, 1000);
-}
 
 ReactDOM.render(
   <React.StrictMode>
