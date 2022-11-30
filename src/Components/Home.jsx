c27f9789ffc36149731847433d37a348ff988baa
Dhruvit Galoriya <dhruvitgaloriya@gmail.com>
2022-11-30 16:35:17 +0530

    fix

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import "../sass/styles.scss";
 import { Link } from "react-router-dom";
+import "../sass/styles.scss";
 class Home extends React.Component {
   render() {
     return (
       <div className="d-flex align-items-center justify-content-center flex-column">
-        <h1 className="display-1">Split Your Bills</h1>
+        <h1 className="display-1">Split Your Billss</h1>
         <Link className="addGroupBtn" to="/add">
           Add group
         </Link>
